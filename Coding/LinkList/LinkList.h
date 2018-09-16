b600e83125b44c16ca707be739bae196ece04415
Sachin Kumar <sachin.kumar@optym.com>
2018-09-16 23:20:09 +0530

    adding LinkList header file

---
+++ Coding/LinkList/LinkList.h
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int val)
+    {
+        this->data = val;
+        this->next = NULL;
+    }
+};
+
+class LinkList
+{
+    Node* node;
+    public:
+        LinkList(int val)
+        {
+            this->node = new Node(val);
+        }
+
+        void PrintList()
+        {
+            Node* temp = node;
+            while(temp!=NULL)
+            {
+                cout<<temp->data<<endl;
+                temp=temp->next;
+            }
+        }
+
+        void AddNode(int val)
+        {
+            Node* temp= node;
+            while(temp->next)
+                temp=temp->next;
+            temp->next = new Node(val);
+        }
+
+        bool DetectLoop()
+        {
+            Node* fast = node;
+            Node* slow = node;
+            while(fast && slow && fast->next)
+            {
+                slow = slow->next;
+                fast = fast->next->next;
+                if(slow==fast)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+};
